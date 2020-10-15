772ec327c9bd769804f5bd9288a6c0e19e8be900
Saheb Sutradhar <sahebsutradhar@Sahebs-MacBook-Pro.local>
2020-10-16 02:10:10 +0530

    added code for spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
   ],
   imports: [
     BrowserModule
